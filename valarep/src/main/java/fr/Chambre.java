b11886fc9f9070190045da1e5c5faa18c567b94c
Cedric <c.caudron59@gmail.com>
2020-02-12 16:38:19 +0100

    add class chambre

---
+++ valarep/src/main/java/fr/Chambre.java
@@ -0,0 +1,19 @@
+package fr;
+
+/**
+ * Chambre
+ */
+public class Chambre {
+
+    private int noChbre;
+    private int joursRestants;
+    private String nomOccupant;
+    enum type {
+        SIMPLE,
+        DOUBLE,
+        SUITE
+    }
+    
+}
+
+
