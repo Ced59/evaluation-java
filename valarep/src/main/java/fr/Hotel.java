a156f92ca28855b520b5e9be8614526c2a9540dc
Cedric <c.caudron59@gmail.com>
2020-02-12 16:58:38 +0100

    add class hotel

---
+++ valarep/src/main/java/fr/Hotel.java
@@ -0,0 +1,16 @@
+package fr;
+
+import java.util.ArrayList;
+
+/**
+ * Hotel
+ */
+public class Hotel {
+
+    private String nom;
+    private ArrayList<Chambre> chambres;
+    private int nbreChbres;
+
+
+    
+}
