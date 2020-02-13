e5bb5ce617cccd6937dc00dd5e9b6b58a5d1c11f
Cedric <c.caudron59@gmail.com>
2020-02-13 13:07:23 +0100

    Je continues car frustré de ne pas avoir fini.
    Add chambres hotel

--- valarep/src/main/java/fr/Hotel.java
+++ valarep/src/main/java/fr/Hotel.java
@@ -11,10 +11,60 @@ public class Hotel {
     private ArrayList<Chambre> chambres;
     private int nbreChbres;
 
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public ArrayList<Chambre> getChambres() {
+        return chambres;
+    }
+
+    public void setChambres(ArrayList<Chambre> chambres) {
+        this.chambres = chambres;
+    }
+
+    public int getNbreChbres() {
+        return nbreChbres;
+    }
+
+    public void setNbreChbres(int nbreChbres) {
+        this.nbreChbres = nbreChbres;
+    }
 
     public Hotel(String nom, int nbreChbres, int nbreEtages) {
         this.nom = nom;
         this.nbreChbres = nbreChbres;
-        
+
+        this.chambres = determineChambres(nbreChbres, nbreEtages);
+    }
+
+    private ArrayList<Chambre> determineChambres(int nbreChbres, int nbreEtages) {
+        int noChbre;
+        int nbreChbrePerEtage = nbreChbres / nbreEtages;
+        ArrayList<Chambre> chambres = new ArrayList<Chambre>();
+
+        for (int i = 1; i <= nbreEtages; i++){
+            for (int j = 0; j < nbreChbrePerEtage; j++){
+                noChbre = Integer.parseInt(i + "" + j);
+
+                if (j < 4){
+                    Chambre chambre = new Chambre(noChbre, "simple");
+                    chambres.add(chambre);
+                }
+                else if (j == nbreChbrePerEtage - 1){
+                    Chambre chambre = new Chambre(noChbre, "suite");
+                    chambres.add(chambre);
+                }
+                else {
+                    Chambre chambre = new Chambre(noChbre, "double");
+                    chambres.add(chambre);
+                }
+
+            }
+        }
     }
 }
