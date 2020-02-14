f1788f25fe3cda0d472ffff6f29218edeb1428c3
Cedric <c.caudron59@gmail.com>
2020-02-14 14:19:18 +0100

    adding hotel operations

--- valarep/src/main/java/fr/App.java
+++ valarep/src/main/java/fr/App.java
@@ -7,8 +7,6 @@ public final class App {
     }
 
     /**
-     * Says hello to the world.
-     *
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
@@ -24,8 +22,7 @@ public final class App {
 
         h.getChambres().get(100).definirOccupant("Cedric", 2);
 
-        System.out.println(h.getChambres().get(100).getNoChbre());
-        System.out.println(h.getChambres().get(100).getNomOccupant());
+        
 
         for (Chambre chambre : h.getChambres()){
 
@@ -35,10 +32,32 @@ public final class App {
                 System.out.println("Occupant chambre n°" + chambre.getNoChbre() + " Type: " + chambre.getType() + " : " + chambre.getNomOccupant());
             }
             
-            
         }
-        
 
+       
+        
+        int nbreJours = 0;
+        passeJoursEtAfficheEtatHotel(h, nbreJours);
+
+        nbreJours = 3;
+        passeJoursEtAfficheEtatHotel(h, nbreJours);
+
+        nbreJours = 15;
+        passeJoursEtAfficheEtatHotel(h, nbreJours);
+        
+    }
+
+
+    private static void passeJoursEtAfficheEtatHotel(Hotel h, int nbreJours) {
+        for (int i = 1; i < nbreJours; i++){
+            h.jourSuivant();
+        }
+
+        System.out.println("");
+        System.out.println(nbreJours + " jours plus tard");
+        System.out.println("Nombre de chambres de l'hotel: " + h.getNbreChbres());
+        System.out.println("Nombre de chambres occupées de l'hotel: " + h.nbreChambresOccupees());
+        System.out.println(h.tauxOccupation() + " % d'occupation de l'hotel.");
     }
 
     public static Hotel tauxOccupation_40_sur_200() {
